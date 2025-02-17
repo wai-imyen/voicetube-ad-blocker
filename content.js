fb93d8bd873f81ca2d46d1f5d9b2ada1ce674d2e
Yen <wai.imyen@gmail.com>
2025-02-17 21:03:41 +0800

    feat: block voicetube promo ad

---
+++ content.js
@@ -0,0 +1,6 @@
+// Remove ads
+var adElement = document.getElementsByClassName('wrapper')[0].nextElementSibling;
+if(adElement) {
+  adElement.remove();
+}
+console.log('VoiceTube ad blocker is activated.');
